91a3e6fc1363ba5dd5251939d59a30443fdb25e7
PrabhatJadab <pkjadab@gmail.com>
2022-01-18 20:54:07 +0530

    first commit

---
+++ src/test/java/com/example/jenkins/SpringJenkinsApplicationTests.java
@@ -0,0 +1,24 @@
+package com.example.jenkins;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static org.junit.Assert.assertEquals;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+class SpringJenkinsApplicationTests {
+
+	public static Logger logger = LoggerFactory.getLogger(SpringJenkinsApplicationTests.class);
+
+	@Test
+	void contextLoads() {
+		logger.info("Test Case Executed..");
+		assertEquals(true, true);
+	}
+
+}
