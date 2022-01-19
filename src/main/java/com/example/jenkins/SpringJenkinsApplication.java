0a36ff3d2d0004a5cba71c842e1142b781d1885c
PrabhatJadab <pkjadab@gmail.com>
2022-01-19 11:26:38 +0530

    Updated source code

--- src/main/java/com/example/jenkins/SpringJenkinsApplication.java
+++ src/main/java/com/example/jenkins/SpringJenkinsApplication.java
@@ -1,20 +1,32 @@
 package com.example.jenkins;
 
+
+import javax.annotation.PostConstruct;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringJenkinsApplication {
-
+	
 	public static Logger logger = LoggerFactory.getLogger(SpringJenkinsApplication.class);
 
+	/*@GetMapping("/ping")
+	public String message() {
+		return "Wao!! Application Deployed successfully in SAP Cloud..";
+	}*/
+	
+	@PostConstruct
 	public void init() {
-		logger.info("Application Started...");
+		logger.info("Application Started.");
 	}
-
 	public static void main(String[] args) {
+		logger.info("Application Executed.");
 		SpringApplication.run(SpringJenkinsApplication.class, args);
 	}
 
--- src/test/java/com/example/jenkins/SpringJenkinsApplicationTests.java
+++ src/test/java/com/example/jenkins/SpringJenkinsApplicationTests.java
@@ -1,25 +1,23 @@
 package com.example.jenkins;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import static org.junit.Assert.assertEquals;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-
-
 @RunWith(SpringRunner.class)
 @SpringBootTest
-class SpringJenkinsApplicationTests {
+public class SpringJenkinsApplicationTests {
 
 	public static Logger logger = LoggerFactory.getLogger(SpringJenkinsApplicationTests.class);
 
 	@Test
-	void contextLoads() {
-		logger.info("Test Case Executed..");
+	public void contextLoads() {
+		logger.info("Test case executing...");
 		assertEquals(true, true);
 	}
 
